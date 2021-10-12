50ebc7724130aff4002f875c49545117799df25a
Ali <92354591+omurbekovic@users.noreply.github.com>
2021-10-12 14:35:12 +0900

    Add files via upload

---
+++ us-states/main.py
@@ -0,0 +1,38 @@
+import turtle
+import pandas
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+a = []
+while len(a) < 50:
+    answer_state = screen.textinput(title=f"Guess the State {len(a)}/50", prompt="Whats another states name ").title()
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in a:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
+    if answer_state in  all_states:
+        a.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
+
+
+
+# def get_mouse_click_cor(x,y):
+#     print(x,y)
+#
+# turtle.onscreenclick(get_mouse_click_cor)
+# turtle.mainloop()
+
